38f52cda9ffa7cfca7014a78c7af90512017a245
Esau-Rodriguez <250387@utxicotepec.edu.mx>
2026-09-24 09:00:48 -0600

    Navegar a la pantalla inicio

--- app/src/main/java/com/example/miprimerlogin/RecuperarPassword.kt
+++ app/src/main/java/com/example/miprimerlogin/RecuperarPassword.kt
@@ -1,6 +1,9 @@
 package com.example.miprimerlogin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,14 @@ class RecuperarPassword : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recuperar_password)
+        val correo = findViewById<EditText>(R.id.txtCorreoRe)
+        val regresar = findViewById<TextView>(R.id.lblRegresar)
+
+        regresar.setOnClickListener {
+            val activityRegresar = Intent(this, Login::class.java)
+            startActivity(activityRegresar)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
